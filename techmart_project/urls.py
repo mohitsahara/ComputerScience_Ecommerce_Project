843bebc5741190a69d0047f4232e37cc7616abd2
mohitsahara <mohitsaharanjaat438@gmail.com>
2026-06-10 21:23:43 +0500

    Add files via upload

---
+++ techmart_project/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+admin.site.site_header = "TechMart Admin"
+admin.site.site_title = "TechMart Admin Portal"
+admin.site.index_title = "Welcome to TechMart Administration"
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('products.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('cart/', include('cart.urls')),
+    path('orders/', include('orders.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
